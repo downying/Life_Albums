73ba57a9eb56b6d119ada8edad65580e69730e1c
Nayeoonn <107173330+Nayeoonn@users.noreply.github.com>
2024-08-31 10:23:09 +0900

    sql: 유저테이블 샘플데이터

---
+++ SQL/샘플데이터.sql
@@ -0,0 +1,3 @@
+-- 아이디 : user 비밀번호: 123456
+INSERT INTO users (id, pw, name, mail, phone)
+VALUES ('user', '$2a$12$TrN..KcVjciCiz.5Vj96YOBljeVTTGJ9AUKmtfbGpgc9hmC7BxQ92', '야호봉','asdf123@naver.com', '01077884455');
